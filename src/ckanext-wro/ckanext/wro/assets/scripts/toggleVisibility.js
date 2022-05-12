e56b0ab91a22f60f0d198e6af04999b6d0ca4337
Mohab <homab3@gmail.com>
2022-05-12 20:14:27 +0200

    completing conditional fields

--- src/ckanext-wro/ckanext/wro/assets/scripts/toggleVisibility.js
+++ src/ckanext-wro/ckanext/wro/assets/scripts/toggleVisibility.js
@@ -3,30 +3,32 @@
  * to respond to other elements (e.g checkbox Did the Author / Contact organization collect the data?)
  * toggle the visibility of Contact section if checked.
  * 
- * 
+ * ======= the general workflow
+ *         grap things by their ids and show/hide them according to the correspondent checkbox
+ *         change the schema from ckanext-scheming by adding custom presets that hold the js modules.
  * 
  *  ===== how this code can imporove:
  *        currently it uses jquery to get elements from screen, i need to use pubsub for that
  */
 
  "use-strict";
-
+ // keeping this to show that using pubsub didn't work before trying to grap things by ids.   
  ckan.module('ckanext_wro_metadata_form_checkbox_module',function($){
-     return {
-         initialize: function(){
-             $.proxyAll(this, /_on/); 
+    //  return {
+    //      initialize: function(){
+    //          $.proxyAll(this, /_on/); 
             
-            //this.el.on('change', this._onChange)
-             //this.sandbox.subscribe('pub')
-          },
-         _onChange:function(e){
-             console.log("it's changed !", this.el.is(':checked'))
+    //         //this.el.on('change', this._onChange)
+    //          //this.sandbox.subscribe('pub')
+    //       },
+    //      _onChange:function(e){
+    //          console.log("it's changed !", this.el.is(':checked'))
              //this.sandbox.publish('pub', this.el.is(':checked'))
-         },
+//         },
         //  teardown: function(){
         //    this.sandbox.unsubscribe('pub')
         //  }
-     }
+     //}
  });
  
 ckan.module('ckanext_wro_toggle_repeating_field_visibilty', function($){
@@ -41,15 +43,15 @@ ckan.module('ckanext_wro_toggle_repeating_field_visibilty', function($){
     },
     _onAlternatePublish:function(e){
       console.log('checked')
-      contact_fields = $(".getter_contact_person")
-      //console.log('binga',contact_fields)
+      contact_fields = $(".getter_contact_person")   // didn't use this.el because there are muliple fields with the same config
+      contact_fields_label = $('label[for="field-contact_person"]')
       if(e.target.checked){
         contact_fields.hide();
-        $('label[for="field-contact_person"]').hide();
+        contact_fields_label.hide();
       }
       else{
         contact_fields.show();
-        $('label[for="field-contact_person"]').show();
+        contact_fields_label.show();
       }
     }
   
@@ -57,18 +59,25 @@ ckan.module('ckanext_wro_toggle_repeating_field_visibilty', function($){
 });
 
 
-//  ckan.module('tester',function($){
-//    return {
-//      initialize:function(){
-//        console.log('binga')
-//        this.sandbox.subscribe('pub', this._onPub)
-//      },
-//      _onPub:function(){
-//         console.log('binga is published')
-//      },
-//      teardown: function(){
-//       this.sandbox.unsubscribe('pub')
-//     }
-//     }
-//  });
+ ckan.module('ckanext_wro_toggle_data_collection_field',function($){
+    return {
+     initialize:function(){
+      $.proxyAll(this, /_on/)
+       console.log('data collection loaded !')
+       let data_collecton_checkbox = $('#field-did_author_or_contact_organization_collect_the_data-None')
+       data_collecton_checkbox.on('change', this._onChange)
+      },
+      _onChange:function(e){
+        data_collector_field_label = $('label[for="field-data_collection_organization')
+        if(e.target.checked){
+          this.el.hide();
+          data_collector_field_label.hide()
+        }
+        else{
+          this.el.show();
+          data_collector_field_label.show();
+        }
+      }
+  }
+ });
 
