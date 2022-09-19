8fdddc19754566aa36f13699a8474ee33946a282
Mohab <homab3@gmail.com>
2022-09-19 23:50:57 +0200

    adding cloud large files upload modal

--- ckanext-wro/ckanext/wro/assets/scripts/google_cloud_upload_note.js
+++ ckanext-wro/ckanext/wro/assets/scripts/google_cloud_upload_note.js
@@ -17,13 +17,17 @@ ckan.module("google_cloud_upload_modal", function($){
             let close_btn = document.querySelector(".google_cloud_upload_note_modal_btn")
             close_btn.addEventListener("click", this.toggleModal)
 
-
-
+            // close if clicked outside modal
+            modal.addEventListener("click", this.closeModal)
+            // this might change in future, if there are elements inside
+            document.querySelector(".google_cloud_note_container").addEventListener(
+                "click", function(e){e.stopPropagation()}
+            )
         }
         ,
         toggleModal:function(e){
             if(modal.style.display == "none"){
-                modal.style.display = "block"
+                modal.style.display = "flex"
             }
             else{
                 modal.style.display = "none"
@@ -32,8 +36,12 @@ ckan.module("google_cloud_upload_modal", function($){
         ,
         openModal: function(e){
             setTimeout(function(){
-                modal.style.display = "block"
+                modal.style.display = "flex"
             },1000)
+        }
+        , 
+        closeModal: function(e){
+            modal.style.display = "none";
         }
     }
 })
